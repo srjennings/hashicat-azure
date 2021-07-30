00551a95be891e72bd6372e5a6da3ce463c5639f
Steve Jennings <8878289+srjennings@users.noreply.github.com>
2021-07-30 17:30:19 +0000

    fix rg

--- vnet.tf
+++ vnet.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "app.terraform.io/SJENNINGS-training/network/azurerm"
   version = "3.0.1"
 
-  resource_group_name = azurerm_resource_group.test.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
@@ -11,9 +11,4 @@ module "network" {
     Environment = "dev"
     Cost-center = "it"
   }
-}
-
-resource "azurerm_resource_group" "test" {
-  name     = "my-resources"
-  location = "eastus"
 }
